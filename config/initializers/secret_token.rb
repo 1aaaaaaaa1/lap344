8b21a9223f88a67bbf5c74bf572819581faad468
1aaaaaaaa1 <qctrai@yahoo.com.vn>
2013-12-12 12:52:16 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lap344::Application.config.secret_key_base = '2922e2b706222d714ae3b48697740d6e3ee94d20ccef2949057240cf90d6455db04dc1c61b7b4246e466ed425033bfd75aa7bf455e88ad38873c590bcb581ef8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lap344::Application.config.session_store :cookie_store, key: '_lap344_session'
